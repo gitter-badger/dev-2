b9d73c12ce58daedaef1d2400fa43e30b5fa6a38
coni2k <serkanholat@hotmail.com>
2014-03-25 17:25:26 +0100

    master

--- Solutions/Playground/Playground.Engine/Class1.cs
+++ Solutions/Playground/Playground.Engine/Class1.cs
@@ -8,10 +8,6 @@ namespace Playground.Engine
 {
     public class Class1
     {
-        /* now we're editing this, so it becomes diff from github source */
-
-        /* and now after all the updates, it's time to merge with main */
-
-        /* TestBranch */
+        /* Class 1 */
     }
 }
