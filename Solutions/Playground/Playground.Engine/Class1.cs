56dcbb705991fe3913374cf4a93eeb0446a21a4f
coni2k <serkanholat@hotmail.com>
2014-03-25 17:18:51 +0100

    TestBranch commit 2

--- Solutions/Playground/Playground.Engine/Class1.cs
+++ Solutions/Playground/Playground.Engine/Class1.cs
@@ -9,5 +9,7 @@ namespace Playground.Engine
     public class Class1
     {
         /* now we're editing this, so it becomes diff from github source */
+
+        /* and now after all the updates, it's time to merge with main */
     }
 }
